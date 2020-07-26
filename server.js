fc6efc55dbae5e22e7b549a0f66331b8b0f5e592
YuanTingHsieh <yuantinghsieh@gmail.com>
2020-07-25 20:48:12 -0700

    Fix static reference

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("port", process.env.PORT || 3001);
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(static("client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.get("/api/boards", (req, res) => {
